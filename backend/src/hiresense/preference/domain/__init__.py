75c686109668109a4d5133d7a3762ea4f1ca5b13
StevSant <Bryanmenoscal2005@gmail.com>
2026-05-31 02:30:20 -0500

    feat(preference): add FeedbackKind and FeedbackSource enums

---
+++ backend/src/hiresense/preference/domain/__init__.py
@@ -0,0 +1,4 @@
+from hiresense.preference.domain.feedback_kind import FeedbackKind
+from hiresense.preference.domain.feedback_source import FeedbackSource
+
+__all__ = ["FeedbackKind", "FeedbackSource"]

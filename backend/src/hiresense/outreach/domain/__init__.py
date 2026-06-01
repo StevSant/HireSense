deb1f4c85bd2e14939c2c48bcecdc07b26a1caf3
StevSant <Bryanmenoscal2005@gmail.com>
2026-05-31 23:00:34 -0500

    feat(outreach): add OutreachEvent/Kind/Nudge domain models

---
+++ backend/src/hiresense/outreach/domain/__init__.py
@@ -0,0 +1,5 @@
+from hiresense.outreach.domain.outreach_event import OutreachEvent
+from hiresense.outreach.domain.outreach_event_kind import OutreachEventKind
+from hiresense.outreach.domain.outreach_nudge import OutreachNudge
+
+__all__ = ["OutreachEvent", "OutreachEventKind", "OutreachNudge"]

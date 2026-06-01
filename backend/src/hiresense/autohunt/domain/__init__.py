fd2b96a381b892e33ceac67b92c28bfa8f6d4699
StevSant <Bryanmenoscal2005@gmail.com>
2026-05-31 21:23:37 -0500

    feat(autohunt): add AutoHuntService run orchestrator

--- backend/src/hiresense/autohunt/domain/__init__.py
+++ backend/src/hiresense/autohunt/domain/__init__.py
@@ -1,4 +1,5 @@
+from hiresense.autohunt.domain.autohunt_service import AutoHuntService
 from hiresense.autohunt.domain.digest import Digest
 from hiresense.autohunt.domain.digest_entry import DigestEntry
 
-__all__ = ["Digest", "DigestEntry"]
+__all__ = ["AutoHuntService", "Digest", "DigestEntry"]

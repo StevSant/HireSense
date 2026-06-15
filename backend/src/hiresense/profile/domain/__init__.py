61af620191c758dd5dda7b8a7cc671b051f55bab
StevSant <Bryanmenoscal2005@gmail.com>
2026-06-15 14:51:20 -0500

    feat(profile): add CVTranslator LaTeX-preserving translation service

--- backend/src/hiresense/profile/domain/__init__.py
+++ backend/src/hiresense/profile/domain/__init__.py
@@ -1,20 +1,24 @@
 from hiresense.profile.domain.apply_prefill import build_prefill
 from hiresense.profile.domain.apply_profile import ApplyProfile
+from hiresense.profile.domain.cv_translator import CVTranslator
 from hiresense.profile.domain.latex_parser import LaTeXParser
 from hiresense.profile.domain.models import CandidateProfile, CVSection
 from hiresense.profile.domain.pdf_parser import PDFParser
 from hiresense.profile.domain.screening_answer import ScreeningAnswer
 from hiresense.profile.domain.services import ProfileService
 from hiresense.profile.domain.skill_extractor import SkillExtractor
+from hiresense.profile.domain.translation_outcome import TranslationOutcome
 
 __all__ = [
     "ApplyProfile",
     "CVSection",
+    "CVTranslator",
     "CandidateProfile",
     "LaTeXParser",
     "PDFParser",
     "ProfileService",
     "ScreeningAnswer",
     "SkillExtractor",
+    "TranslationOutcome",
     "build_prefill",
 ]

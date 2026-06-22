b6ddc18a85c5c3c6ff0a2741e118026e0e3be70b
StevSant <Bryanmenoscal2005@gmail.com>
2026-06-21 19:25:45 -0500

    feat(scheduler): add repository ports

---
+++ backend/src/hiresense/scheduler/domain/ports/__init__.py
@@ -0,0 +1,4 @@
+from hiresense.scheduler.domain.ports.job_run_repository import JobRunRepository
+from hiresense.scheduler.domain.ports.job_toggle_repository import JobToggleRepository
+
+__all__ = ["JobRunRepository", "JobToggleRepository"]
